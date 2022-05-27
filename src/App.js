a243c2a20d2895fd1c434be17f290a963198c423
Emanuele <Emanuele>
2022-05-27 22:05:59 +0200

    Component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Hello } from "./Hello";
+import { Message } from "./Message";
+
+
+export class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <Hello/>
+                <Message />
+                <Hello />
+            </div>
+        )
+    }
+}
