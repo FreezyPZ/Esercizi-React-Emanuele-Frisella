7e6aba789ae6cf91fcbb4eba0e9ee1450924656a
Emanuele <Emanuele>
2022-07-04 14:47:25 +0200

    fd

--- src/counter.js
+++ src/counter.js
@@ -8,15 +8,15 @@ export class Counter extends React.Component {
 
     constructor(props) {
         super(props)
-
-        setInterval(() => {
+    }
+        intervval=setInterval(() => {
             this.setState((state) => {
                 return {
                     count: state.count + (this.props.incrementAmount)
                 }
             })
         }, this.props.incrementInterval)
-    }
+    
     render() {
         return (
             <div>
@@ -29,5 +29,5 @@ export class Counter extends React.Component {
 Counter.defaultProps = {
     initialValue: 0,
     incrementAmount: 1,
-    incrementInterval: 1000
+    incrementInterval: 2000
 }
